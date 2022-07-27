0ab226c0f157e96d7211f3392693ef47a7a1eaa8
imamyusupb <imamyusufb123@gmail.com>
2022-07-27 10:34:45 +0700

    added signup fragment

--- app/src/main/java/com/codeyuba/foodmarketkotlin/SplashScreenActivity.kt
+++ app/src/main/java/com/codeyuba/foodmarketkotlin/SplashScreenActivity.kt
@@ -1,11 +1,22 @@
 package com.codeyuba.foodmarketkotlin
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.widget.ImageView
 
 class SplashScreenActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash_screen)
+
+        Handler(Looper.getMainLooper()).postDelayed(Runnable {
+            startActivity(Intent(
+                this@SplashScreenActivity,
+                MainActivity::class.java))
+        },3000)
     }
 }
